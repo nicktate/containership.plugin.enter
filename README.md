afaadace2b6b2e793fed2e2de8fd3ac0ca4dfb4f
Norman Joyner <norman.joyner@gmail.com>
2015-03-23 22:35:17 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+enter
+==================
+
+##About
+
+###Description
+A CLI plugin used to enter ContainerShip containers
+
+###Author
+ContainerShip Developers - developers@containership.io
+
+##Usage
+
+###Install
+`containership plugin add ssh`
+
+###Update
+`containership plugin update ssh`
+
+###Remove
+`containership plugin remove ssh`
+
+##Contributing
+Pull requests and issues are encouraged!
---
+++ application.js
@@ -0,0 +1,53 @@
+var fs = require("fs");
+var _ = require("lodash");
+var ContainershipPlugin = require("containership.plugin");
+var Container = require([__dirname, "lib", "container"].join("/"));
+
+module.exports = new ContainershipPlugin({
+    type: "cli",
+
+    initialize: function(){
+        var self = this;
+
+        return {
+            init: function(options){
+                if(_.has(options, "container-id")){
+                    var container = new Container({
+                        application: options.application,
+                        container_id: options["container-id"],
+                        api_url: self.config["api-url"],
+                        ssh_username: self.config["ssh-username"]
+                    });
+
+                    container.load(function(err){
+                        if(err)
+                            console.log(err.message);
+                        else{
+                            container.enter(function(err){
+                                if(err)
+                                    console.log(err);
+                            });
+                        }
+                    });
+                }
+            },
+
+            options: {
+                application: {
+                    position: 1,
+                    help: "ContainerShip application name",
+                    required: true
+                }
+            },
+
+            configure_options: {
+                "ssh-username": {
+                    help: "username used to ssh into ContainerShip hosts",
+                    required: true
+                }
+            }
+        }
+    },
+
+    reload: function(){}
+});
